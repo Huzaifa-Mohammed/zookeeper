d3779e1b1ed71d035cc8929e7e7df27502b44cd6
Huzaifa Mohammed <huzirizwan@gmail.com>
2022-01-13 20:04:09 -0500

    specific search query updates

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const req = require('express/lib/request');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -40,7 +41,10 @@ function filterByQuerry(query, animalsArray) {
     return filteredResults;
 };
 
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -51,6 +55,15 @@ app.get('/api/animals', (req, res) => {
 
 
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
